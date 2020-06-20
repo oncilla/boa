f7fa2e6418ee6a072f899ab38fc9074b2966d669
Oncilla <domi.roos@gmail.com>
2020-06-20 18:22:58 +0200

    readme: improve contents
    Add shields to readme.
    Add description on how to install and use boa.
    Add `boa` package with helpers.

---
+++ pkg/boa/util.go
@@ -0,0 +1,22 @@
+// Copyright 2020 oncilla
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package boa
+
+// Pather wraps a string to implement the Pather interface.
+type Pather string
+
+// CommandPath returns the wrapped string
+func (p Pather) CommandPath() string {
+	return string(p)
+}
