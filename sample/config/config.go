4f6caf699ce4abd37ccad1a04e08bf2d54fd1448
Oncilla <domi.roos@gmail.com>
2020-09-26 22:40:17 +0200

    sample: add app showcasing configuration
    Add a simple sample application that loads a configuration with viper
    and displays the result.

---
+++ sample/config/config.go
@@ -0,0 +1,119 @@
+// Copyright 2020 oncilla
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
+
+	"github.com/oncilla/boa/pkg/boa"
+)
+
+func main() {
+	cmd := &cobra.Command{
+		Use:   "config <config-file>",
+		Short: "A sample application with config parsing",
+		Long: `This is a sample application that showcases config parsign with the help of boa.
+
+This application loads the configuration based on the following precedence:
+
+1. Environment variable
+2. Configuration file
+3. Default values
+
+The default configuration is:
+
+    db:
+	  user: user
+	  password: password
+	addr: 127.0.0.1:8080
+
+The config file can be passed as as a command line argument.
+
+Environment variables are prefixed With 'SAMPLE_':
+
+SAMPLE_DB_USER=secure
+`,
+		SilenceErrors: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			v := viper.New()
+			v.SetEnvPrefix("sample")
+			v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+			// Define the configuration struct.
+			cfg := &Config{
+				DB: DB{
+					User:     "user",
+					Password: "password",
+				},
+				Addr: &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 8080},
+			}
+
+			// Set the default values for the configuration.
+			if err := boa.SetDefaults(v, cfg); err != nil {
+				return err
+			}
+
+			// Bind the environment variables based on the configuration struct.
+			if err := boa.BindEnv(v, cfg); err != nil {
+				return err
+			}
+
+			// Set hooks to parse the TCP address.
+			hooks := viper.DecodeHook(mapstructure.ComposeDecodeHookFunc(boa.DefaultDecodeHooks()...))
+
+			// If any configuration files are passed, parse them.
+			if len(args) > 0 {
+				for _, file := range args {
+					v.SetConfigFile(file)
+				}
+
+				if err := v.ReadInConfig(); err != nil {
+					return err
+				}
+			}
+
+			// Pares the configuration.
+			var out Config
+			if err := v.Unmarshal(&out, hooks); err != nil {
+				return err
+			}
+
+			// Display the parsed configuration.
+			enc := yaml.NewEncoder(os.Stdout)
+			return enc.Encode(out)
+		},
+	}
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+type Config struct {
+	DB   DB           `mapstructure:"db"`
+	Addr *net.TCPAddr `mapstructure:"addr"`
+}
+
+type DB struct {
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+}
